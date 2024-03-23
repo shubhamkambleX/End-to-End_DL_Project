a09244d34e1b67e900a3e788335a0d8e57fce028
shubhamkambleX <196shubham@gmail.com>
2024-03-23 21:58:19 +0530

    data ingestion component added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # End-to-End_DL_Project
+
+
+## Workflows
+1) config.yaml
+2) params.yaml
+3) entity folder
+4) configuration manager in src folder
+5) components
+6) piplines
+7) main.py
+8) dvc.yaml
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from CnnClassifier import logger
+from CnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("Welcome to cnnClassifier")
+STAGE_NAME =  "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n x====================================x")
+except Exception as e:
+    logger.exception(e)
+    raise e 
